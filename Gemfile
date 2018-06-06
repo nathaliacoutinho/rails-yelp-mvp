95721099640cc32e55a8e7bad38037500b892e31
nathalia coutinho <nathccoutinhos@gmail.com>
2018-06-06 13:50:53 +0200

    created model and controllers for restaurants and reviews

--- Gemfile
+++ Gemfile
@@ -32,6 +32,11 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'bootstrap-sass'
+gem 'font-awesome-sass', '~> 5.0.9'
+gem 'simple_form'
+gem 'autoprefixer-rails'
+gem 'jquery-rails'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
@@ -56,6 +61,7 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
+  gem 'rails-controller-testing'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+restaurants = Restaurant.create(
+  [
+    {
+      name: 'Paris 6',
+      address: 'Sao Paulo, Brazil',
+      phone_number: '119827382341',
+      category: 'french'
+    },
+
+    {
+      name: 'China in Box',
+      address: 'Av Leopoldina, 55',
+      phone_number: '118772681',
+      category: 'chinese'
+    },
+
+    {
+      name: 'La Cantina',
+      address: 'Oscar Freire, 35',
+      phone_number: '148338322',
+      category: 'italian'
+    },
+
+    {
+      name: 'Sushi',
+      address: 'Damrak, 3',
+      phone_number: '064738292',
+      category: 'japanese'
+    },
+
+    {
+      name: 'Lekker',
+      address: 'Oranjeplein, 4',
+      phone_number: '069748592',
+      category: 'belgian'
+    }
+  ])
